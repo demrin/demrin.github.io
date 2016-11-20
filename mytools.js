890d835d551532363e9d89c79e312e8cfeae51a9
demrin <857006354@qq.com>
2016-11-20 23:21:09 +0800

    mytools update in 11.20

--- mytools.js
+++ mytools.js
@@ -1,15 +1,22 @@
 /**
  * Created by demrin on 2016/11/11.
  */
-function carryMyAjax(url,type,requestData,succeed,failed){
+function startAjax(url,type,requestData,succeed,failed){
     if(type == 'get' || type == 'GET'){
         var temstrings = '&';
         for(var i in requestData){
             if(request[i] == 'underfined' || typeof(requestData[i]) == 'underfined'){
                 temstrings = temstrings + i + '=&'
             }else{
-                temstrings
+                temstrings = temstrings + i + '=' + requestData[i] + '&'
             }
         }
+        if(url.indexOf('?')){
+            url = url + temstrings
+        }else{
+            url = url + '?' + temstrings
+        }
+    }else if(type == 'post' || type == 'POST'){
+        var temstrings = new Array()
     }
 }
